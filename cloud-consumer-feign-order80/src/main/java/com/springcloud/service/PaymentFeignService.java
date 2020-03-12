02741852e8dd2636f4f4dbfb07f9cd0b7918aa30
ShiYanZZhang-1 <cnzhangshiyan@163.com>
2020-03-12 12:25:22 +0800

    Hystrix相关配置应用

---
+++ cloud-consumer-feign-order80/src/main/java/com/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,20 @@
+package com.springcloud.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import top.shiyana.springcloud.entities.CommonResult;
+import top.shiyana.springcloud.entities.Payment;
+
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+    @GetMapping(value = "/payment/feign/timeout")
+    String paymentFeignTimeOut();
+
+}
